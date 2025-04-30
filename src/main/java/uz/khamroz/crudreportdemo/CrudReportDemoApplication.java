4602a3657783fb6a2b66808263a7164015d749be
Khamroz <khamroz_001@mail.ru>
2025-04-30 00:41:46 -0700

    First

---
+++ src/main/java/uz/khamroz/crudreportdemo/CrudReportDemoApplication.java
@@ -0,0 +1,13 @@
+package uz.khamroz.crudreportdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudReportDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudReportDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uz/khamroz/crudreportdemo/CrudReportDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.khamroz.crudreportdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudReportDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
